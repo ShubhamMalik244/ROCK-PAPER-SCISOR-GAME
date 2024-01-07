083919395278ecec66f12768e5102452d2f4160b
ShubhamMalik244 <120198030028shubham@gmail.com>
2024-01-07 21:26:04 +0530

    leaderBoard is added

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ export default function App() {
   return (
     <main className="appContainer">
 
-
-         <section className="overlayPage">
+        
+         <section className="overlayPage hide">
           <div className="box">
             <nav>
               <h1>RULES</h1>
@@ -18,7 +18,20 @@ export default function App() {
           </div>
          </section>
 
+         <section className="gamePage">
 
+               <div className="leadBoard">
+                <img src="images/logo-bonus.svg" alt="logo"></img>
+                <div className="scoreContainer">
+                  <h2>SCORE</h2>
+                  <h1>12</h1>
+                </div>
+               </div>
+
+               <div className="gameBtnContainer">
+
+               </div>
+         </section>
     </main>
   );
 }
