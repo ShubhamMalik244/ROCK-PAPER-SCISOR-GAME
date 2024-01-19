7c5ab835cc1d5c5a85877d220f6239fcb7520052
ShubhamMalik244 <120198030028shubham@gmail.com>
2024-01-19 21:29:36 +0530

    styling complete

--- src/App.js
+++ src/App.js
@@ -1,44 +1,51 @@
-import PlayBtn from './Components/PlayBtn';
+import PlayBtn from "./Components/PlayBtn";
 
 export default function App() {
-
-
-
-
-
   return (
     <main className="appContainer">
+      <section className="overlayPage hide">
+        <div className="box">
+          <nav>
+            <h1>RULES</h1>
+            <button></button>
+          </nav>
+          <img src="images/image-rules-bonus.svg" alt="rules"></img>
+        </div>
+      </section>
 
-        
-         <section className="overlayPage hide">
-          <div className="box">
-            <nav>
-              <h1>RULES</h1>
-              <button></button>
-            </nav>
-            <img src="images/image-rules-bonus.svg" alt="rules"></img>
+      <section className="gamePage">
+        <div className="leadBoard">
+          <img src="images/logo-bonus.svg" alt="logo"></img>
+          <div className="scoreContainer">
+            <h2>SCORE</h2>
+            <h1>12</h1>
           </div>
-         </section>
+        </div>
 
-         <section className="gamePage ">
+        <div className="gameBtnContainer hide">
+          <PlayBtn btnType="rock rock-P smallBtn" />
+          <PlayBtn btnType="paper paper-P smallBtn" />
+          <PlayBtn btnType="scissors scissors-P smallBtn" />
+          <PlayBtn btnType="lizard lizard-P smallBtn" />
+          <PlayBtn btnType="spock spock-P smallBtn" />
+        </div>
 
-               <div className="leadBoard">
-                <img src="images/logo-bonus.svg" alt="logo"></img>
-                <div className="scoreContainer">
-                  <h2>SCORE</h2>
-                  <h1>12</h1>
-                </div>
-               </div>
+        <div className="resultContainer ">
+          <PlayBtn btnType="scissors bigBtn user" />
 
-               <div className="gameBtnContainer">
-                 <PlayBtn btnType='rock'/>
-                 <PlayBtn btnType='paper'/>
-                 <PlayBtn btnType='scissors'/>
-                 <PlayBtn btnType='lizard'/>
-                 <PlayBtn btnType='spock'/>
-               </div>
-         </section>
+          <div className="reset hide">
+            <h1>YOU LOSE</h1>
+            <button>PLAY AGAIN</button>
+          </div>
+
+          <PlayBtn btnType="emptyBtn bigBtn" />
+          <PlayBtn btnType="paper bigBtn comp hide" />
+        </div>
+
+        <div className="ruleBtnContainer">
+          <button className="ruleBtn">RULES</button>
+        </div>
+      </section>
     </main>
   );
 }
-
--- src/Components/PlayBtn.js
+++ src/Components/PlayBtn.js
@@ -1,3 +1,3 @@
 export default function PlayBtn({btnType}) {
-  return <div className={"playBtn " + btnType}></div>;
+  return <div className={btnType}></div>;
 }
